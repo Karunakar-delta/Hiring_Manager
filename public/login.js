308a2adaba817829365ab360be73ccbb3a6e6f06
Karunakar <Talatamkarun@gmail.com>
2025-05-26 11:54:03 +0530

    Update login

--- public/login.js
+++ public/login.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const password = document.getElementById("password").value;
 
     try {
-      const response = await fetch("/auth/login", {
+      const response = await fetch("https://hiring-manager-lemon.vercel.app/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
